355b80748278ca3e65bb06c5b8fd31e8bbe925cf
lin-xin <2981207131@qq.com>
2018-04-16 10:42:33 +0800

    '生成v2.0.0版本'

--- src/version.js
+++ src/version.js
@@ -1,5 +1,5 @@
 /*!
- * sChart JavaScript Library v1.1.0
+ * sChart JavaScript Library v2.0.0
  * http://blog.gdfengshuo.com/example/sChart/ | Released under the MIT license
- * Date: 2018-04-15T18:59Z
+ * Date: 2018-04-16T18:59Z
  */
