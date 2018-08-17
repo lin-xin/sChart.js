0d86ee8d55c2c60b5db84fb141eed3bf546247c7
lin-xin <2981207131@qq.com>
2018-08-17 17:30:30 +0800

    '饼状图/环形图添加showValue配置项'

--- src/version.js
+++ src/version.js
@@ -1,5 +1,5 @@
 /*!
- * sChart JavaScript Library v2.0.0
+ * sChart JavaScript Library v2.0.1
  * http://blog.gdfengshuo.com/example/sChart/ | Released under the MIT license
  * Date: 2018-04-16T18:59Z
  */
